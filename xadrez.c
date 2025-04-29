52a8ee578e70cd65e679e95551f4542818efb5ae
André Luís Scuccuglia da Costa <79751949+AndreCosta2021@users.noreply.github.com>
2025-04-29 13:07:20 +0000

    Desafio movimento peças do xadrez - Novato

--- xadrez.c
+++ xadrez.c
@@ -3,19 +3,112 @@
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+// Aluno: André Luís Scuccuglia da Costa
+// Matrícula: 202503001421
+// Curso: ADS
+// Turma: 2025.1
 
 int main() {
     // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    // Declaração das variáveis
+
+    // Variáveis para coletar as informações das movimentações das peças tanto na vertical (_vert) 
+    // quanto na horizontal (_hor)
+    int bispo_vert, bispo_hor, rainha_vert, rainha_hor, torre_vert, torre_hor, mov_bispo, mov_rainha, mov_torre;
+
+    // Inicialização das variáveis
+    bispo_vert = 5;
+    bispo_hor = 5;
+    rainha_vert = 0;
+    rainha_hor = -8;
+    torre_hor = 5;
+    torre_vert = 0;
+    mov_bispo = 0;
+    mov_rainha = 0;
+    mov_torre = 0;
 
     // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // Escolhido para a movimentação do Bispo a estrutura de repetição while
+
+    printf("O movimento do bispo foi: \n");
+    
+    if (bispo_vert == 0 && bispo_hor != 0)
+        {
+            while (mov_bispo <= 5)
+            {
+                printf("direta %d\n", mov_bispo);
+                mov_bispo ++;
+            }
+        } else if (bispo_vert != 0 && bispo_hor == 0)
+        {
+            while (mov_bispo <= 5)
+            {
+                printf("Acima %d\n", mov_bispo);
+                mov_bispo ++;
+            }
+        } else
+        {
+            while (mov_bispo <= 5)
+            {
+                printf("direita %d, Acima %d\n", mov_bispo, mov_bispo);
+                mov_bispo ++;
+            }
+        };
 
     // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    // Escolhido para a movimentação do Torre a estrutura de repetição do while
 
+    // Analisando as opções de movimento
+    // 1ª análise ==> Movimento apenas na hozizontal
+    
+    printf("\nO movimento do torre foi: \n");
+    if (torre_vert == 0 && torre_hor != 0)
+        {
+            do {
+                printf("direta %d\n", mov_torre);
+                mov_torre ++;
+            } while (mov_torre <= 5);
+        
+        } else if (torre_vert != 0 && torre_hor == 0) // 2ª análise ==> Movimento apenas na vertical
+        {
+            do {
+                printf("Acima %d\n", mov_torre);
+                mov_torre ++;
+                } while (mov_torre <= 5);
+
+        } else //3ª análise ==> movimentos em ambas as direções
+        {
+            printf("Movimento inválido para esta peça!\n");
+
+        };
     // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // Escolhido para a movimentação da Rainha a estrutura de repetição FOR
+
+    // Analisando as opções de movimento
+    // 1ª análise ==> Movimento apenas na hozizontal
+    
+    printf("\nO movimento do rainha foi: \n");
+    if (rainha_vert == 0 && rainha_hor != 0)
+        {
+            for (mov_rainha = 0; mov_rainha <= 8; mov_rainha++) 
+            {
+                printf("esquerda %d\n", mov_rainha);
+            }
+        
+        } else if (rainha_vert != 0 && rainha_hor == 0) // 2ª análise ==> Movimento apenas na vertical
+        {
+            for (mov_rainha = 0; mov_rainha <=8; mov_rainha++)
+             {
+                printf("Acima %d\n", mov_rainha);
+            } 
+
+        } else //3ª análise ==> movimentos em ambas as direções
+        {
+            for (mov_rainha = 0; mov_rainha <=8; mov_rainha++)
+            {
+                printf("esquerda %d, Acima %d\n", mov_rainha, mov_rainha);
+            }
+        };
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
